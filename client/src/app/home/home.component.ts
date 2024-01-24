ee0ce0f0d0d84c064473ef8957aabcb3c4eb36df
bugasss <bachisantonio85@gmail.com>
2024-01-24 18:56:34 +0100

    end of section 9

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -10,23 +10,13 @@ export class HomeComponent implements OnInit {
   registerMode = false;
   users: any;
 
-  constructor(private http: HttpClient) {}
+  constructor() {}
 
   ngOnInit(): void {
-    this.getUser();
   }
 
   registerToggle(){
     this.registerMode = !this.registerMode;
-  }
-
-  
-  getUser(){
-    this.http.get('https://localhost:5001/api/users').subscribe({
-      next: response => this.users = response,
-      error: error => console.log(error),
-      complete:() => console.log('Request has completed')
-    })
   }
 
   cancelRegisterMode(event: boolean){
